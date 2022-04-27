391e0e88328dabd6b3a6cb74de193a3963ddb251
Selaelo <selaelolemekoana@gmail.com>
2022-04-27 21:07:12 +0200

    The product add page is completed without cancel button styling working or other functionalities. Next is solving the cancel button styling then inserting user input to the database using MySQL.

--- addProduct.php
+++ addProduct.php
@@ -1,73 +1,86 @@
 <?php
 
-
 ?>
 
-    <?php include('header.php'); ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
 
-    
-    <form id="product_form">
-        <div class="mb-3">
-            <label for="InputSKU" class="form-label">SKU</label>
-            <input type="sku" class="form-control" id="sku" aria-describedby="sku">
+    <div class="fixed-header">
+        <h1><a href="#">Product Add</a></h1>
+        <div class="container">
+            <input type="submit" value="Save" name="submit">
+            <input type="button" value="Cancel" name="cancel">
         </div>
-        <div class="mb-3">
-            <label for="InputName" class="form-label">Name</label>
-            <input type="name" class="form-control" id="name" required type="text">
-        </div>
-        <div class="mb-3">
-            <label for="InputPrice" class="form-label">Price ($)</label>
-            <input type="price" class="form-control" id="price" required type="number">
-        </div>
-        <div class="mb-3">
-            <label for="InputPrice" class="form-label">Type Switcher</label>
-            <select class="form-select" id="productType" aria-label="Default select example">
+    </div>
+
+    <div class="add-product">
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" id="product_form" method="post" >
+            <label for=""> SKU </label>
+            <input type="text" name="sku" id="sku" required>
+            <br></br>
+
+            <label for="">Name</label>
+            <input type="text" name="name" id="name" required>
+            <br></br>
+
+            <label for="">Price ($)</label>
+            <input type="text" name="price" id="price" required>
+            <br></br>
+
+            <label for="" class="">Type Switcher</label method="post">
+            <select name="productType" class="" id="productType" required>
                 <option selected>Type Switcher</option>
                 <option value="dvd">DVD</option>
                 <option value="book">Book</option>
                 <option value="furniture">Furniture</option>
-            </select>        
-        </div>
+            </select>
+            <br></br>
 
-        <div class="row g-3 align-items-center">
             <div class="col-auto" id="dvd">
-                <label for="inputPassword6" id="size" class="col-form-label">Size (MB)</label>
-                <input type="password" id="size" class="form-control" aria-describedby="passwordHelpInline">
-                <div id="emailHelp" class="form-text">Please provide the size in megapixel.</div>
+                <label for="" id="size" class="">Size (MB)</label>
+                    <input type="text" id="size" class="" required>
             </div>
-        </div>
-        <div class="row g-3 align-items-center">
-            <div class="col-auto" id="furniture">
-                <label for="inputPassword6" id="height" class="col-form-label">Height (CM)</label>
-                <input type="password" id="height" class="form-control" aria-describedby="passwordHelpInline">
-                <label for="inputPassword6" id="width" class="col-form-label">Width (CM)</label>
-                <input type="password" id="width" class="form-control" aria-describedby="passwordHelpInline">
-                <label for="inputPassword6" id="length" class="col-form-label">Length (CM)</label>
-                <input type="password" id="length" class="form-control" aria-describedby="passwordHelpInline">
-                <div id="emailHelp" class="form-text">Please provide dimensions in H*W*L.</div>
-            </div>
-        </div>
-        <div class="row g-3 align-items-center">
             <div class="col-auto" id="book">
-                <label for="inputPassword6" id="weight" class="col-form-label">Weight (KG)</label>
-                <input type="password" id="weight" class="form-control" aria-describedby="passwordHelpInline">
-                <div id="emailHelp" class="form-text">Please provide the weight in kilograms.</div>
+                <label for="" id="weight" class="">Weight (KG)</label>
+                    <input type="text" id="weight" class="" required>
             </div>
-        </div>
-    </form>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script>
+            <div class="col-auto" id="furniture">
+                <label for="" id="height" class="">Height (CM)</label>
+                    <input type="text" id="height" class="" required>
+                    <br></br>
+                <label for="" id="width" class="">Width (CM)</label>
+                    <input type="text" id="width" class="" required>
+                    <br></br>
+                <label for="" id="length" class="">Length (CM)</label>
+                    <input type="text" id="length" class="" required>
+                    <br></br>
+            </div>
 
-        $(document).ready(function() {
-            $("#productType").on('change',function() {
-                $(".col-auto").hide();
-                $("#" + $(this).val()).fadeIn(700);
-            }).change();
-        });
-    </script>
+            
+        </form>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script>
 
-    <?php include('footer.php'); ?>
+            $(document).ready(function() {
+                $("#productType").on('change',function() {
+                    $(".col-auto").hide();
+                    $("#" + $(this).val()).fadeIn(700);
+                }).change();
+            });
+        </script>
 
-<!-- </body> -->
+    </div>
+
+    <div class="fixed-footer">
+        <div class="container">Copyright &copy; 2022 Selaelo Lemekoana</div>        
+    </div>
+</body>
 </html>
